05b8efb26042ff314610b8601c50254750ad03e0
backend.dude <71830788+rondinabrybry@users.noreply.github.com>
2024-11-21 17:25:34 +0800

    disable register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -95,7 +95,7 @@
                 </div>
                 <button class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-200">{{ __('Log in') }}</button>
             </form>
-            <p class="mt-4 text-center text-gray-600">{{ __('Forgot your password?') }} <a class="text-blue-500" href="{{ route('register') }}">{{ __('Sign up') }}</a></p>
+            {{-- <p class="mt-4 text-center text-gray-600">{{ __('Forgot your password?') }} <a class="text-blue-500" href="{{ route('register') }}">{{ __('Sign up') }}</a></p> --}}
         </div>
     </div>
     
