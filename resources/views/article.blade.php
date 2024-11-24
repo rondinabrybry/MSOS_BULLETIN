833838ce360972308050979b4564d9acb045467c
backend.dude <71830788+rondinabrybry@users.noreply.github.com>
2024-11-24 08:31:29 +0800

    art ads

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -30,18 +30,17 @@
         </article>
 
         
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6444619677143056"
-    crossorigin="anonymous"></script>
-    <!-- responsive -->
-    <ins class="adsbygoogle"
-        style="display:block"
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6444619677143056"
+        crossorigin="anonymous"></script>
+   <ins class="adsbygoogle"
+        style="display:block; text-align:center;"
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
         data-ad-client="ca-pub-6444619677143056"
-        data-ad-slot="9969715782"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-    <script>
+        data-ad-slot="6030470771"></ins>
+   <script>
         (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+   </script>
 
     </main>
 
