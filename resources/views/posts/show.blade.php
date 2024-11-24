c9dc6755cc2a6102501e53437a8d9c09e2c107ba
backend.dude <71830788+rondinabrybry@users.noreply.github.com>
2024-11-24 19:39:53 +0800

    appid

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,6 +9,8 @@
     <meta name="description" content="{{ $metaDescription ?? 'Explore the latest news, academic programs, student resources, and campus life at Cebu Technological University - Danao Campus. Stay informed about admissions, scholarships, events, and opportunities for student success in a vibrant university community' }}">
     <meta name="keywords" content="{{ $metaKeywords ?? 'university, higher education, college life, campus, academic programs, student resources, faculty, research, scholarships, admissions, university news, student success, study tips, campus events, degree programs, online courses, university rankings, international students, student organizations, alumni, university community, career services, campus facilities, financial aid, student engagement, academic excellence, student support, university events, campus culture, educational opportunities, university guide, study abroad, university life, campus tours, student housing, university admissions process' }}">
 
+    <meta property="fb:app_id" content="1928575444318418">
+
     <meta property="og:type" content="article">
     <meta property="og:title" content="{{ $post->title }}">
     <meta property="og:description" content="{{ \Illuminate\Support\Str::limit(strip_tags($post->content), 200) }}">
